154fd27c6c6bcf972a3199b55d750bdad1667151
msagres <m_costa3@etu-webschoolfactory.fr>
2022-05-15 16:24:36 +0200

    add dev dependency // at The Login Page

--- src/main/java/com/mgrs/angularspringsecuritycourse/AngularSpringSecurityCourseApplication.java
+++ src/main/java/com/mgrs/angularspringsecuritycourse/AngularSpringSecurityCourseApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class AngularSpringSecurityCourseApplication {
 
   public static void main(String[] args) {
